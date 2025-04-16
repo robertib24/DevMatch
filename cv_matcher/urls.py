7095a078403e875d983813c28353b5c7a6b5cd0d
Robert <128862496+robertib24@users.noreply.github.com>
2025-04-16 19:21:43 +0300

    fix backend

--- cv_matcher/urls.py
+++ cv_matcher/urls.py
@@ -18,9 +18,11 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from django.views.generic import RedirectView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('api.urls')),
+    path('', RedirectView.as_view(url='/api/', permanent=False)),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
